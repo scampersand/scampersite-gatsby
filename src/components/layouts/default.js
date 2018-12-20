3db85eed7c5c44bcda3ac87f8d6145d0ff908f60
Amy Griffis <amy@scampersand.com>
2018-12-20 15:28:09 -0500

    typography.js comes with its own reset

--- src/components/layouts/default.js
+++ src/components/layouts/default.js
@@ -2,12 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import SEO from '../seo'
 
-// normalize comes from npm
-import 'normalize.css'
-
-// our own simple reset
-import './reset.css'
-
 const Layout = ({children, keywords, title}) => (
   <>
     <SEO title={title} keywords={keywords} />
