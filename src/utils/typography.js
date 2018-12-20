2c7d8836e513586e4d2e89be86a2f8890c5e6d6a
Amy Griffis <amy@scampersand.com>
2018-12-20 15:49:22 -0500

    tweak header styles

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,10 +1,17 @@
 import Typography from 'typography'
+import verticalRhythm from 'compass-vertical-rhythm'
 
 const typography = new Typography({
   baseFontSize: '18px',
   baseLineHeight: 1.666,
   headerFontFamily: ['brother-1816', 'sans-serif'],
   bodyFontFamily: ['brother-1816', 'sans-serif'],
+  headerWeight: '400',
+  overrideStyles: ({ rhythm }) => ({
+    'h1,h2,h3,h4,h5,h6': {
+      marginBottom: rhythm(1/2),
+    },
+  }),
 })
 
 export default typography
