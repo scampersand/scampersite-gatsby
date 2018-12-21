1704036edf8ded1e2ac06498b92d0885d3643879
Aron Griffis <aron@scampersand.com>
2018-12-21 07:30:20 -0500

    reindent

--- src/pages/index.js
+++ src/pages/index.js
@@ -141,70 +141,70 @@ const IndexPage = ({
   projectImages = namedImages(projectImages)
   clientLogos = namedImages(clientLogos)
   return (
-  <Landing title={siteMetadata.title}>
-    <Container>
-      <Header />
-      <Intro>{siteMetadata.description}</Intro>
-      <Section>
-        <h1>Work</h1>
-        <Project
-          title="Tizra Quickstart"
-          image={projectImages['tizra-quickstart']}
-        >
-          <Project.Description>
-            Tizra brought in Scampersand to improve the configurability and
-            mobile experience of their out-of-the-box theme. Our job was to make
-            these improvements on a small budget within an existing codebase. We
-            applied some creative thinking and technical expertise to provide
-            Tizra with a solution that speeds up their onboarding and allows
-            them to pursue scaling their customer base.
-          </Project.Description>
-        </Project>
-        <Project
-          title="Appsembler Reporting"
-          image={projectImages['appsembler-reports']}
-        >
-          <Project.Description>
-            Appsembler wanted to know how many learners were actively using
-            their Open edX LMS. To give them insight into engagement and
-            customer ROI, we built a secure usage tracker and then backfilled it
-            with historical data. Previously inaccessible information became
-            charts revealing trends and entirely new conversations for the
-            customer success team to pursue. Our work on this ultimately became
-            the foundation for Appsembler's customer-facing reporting tool,
-            Figures.
-          </Project.Description>
-          <Project.Testimonial from="Aaron Beals, Appsembler VP Eng">
-            I have to say, the amount you were able to get done with two people
-            in this timeframe is amazing.
-          </Project.Testimonial>
-        </Project>
-        <Project
-          title="Einstein Search"
-          image={projectImages['einstein-search']}
-        >
-          <Project.Description>
-            We worked with the Einstein Papers Project to improve the
-            searchability of their online archive. Based on extensive feedback
-            from researchers, we created a new UI that matches their terminology
-            and workflow. The new interface is also mobile-friendly and includes
-            explanations of commonly used abbreviations to make the archive more
-            approachable to new users.
-          </Project.Description>
-          <Project.Award name="SIIA CODiE Award finalist" />
-        </Project>
-      </Section>
-      <Section>
-        <h1>Clients</h1>
-        <LogoGrid logos={clientLogos} />
-      </Section>
-      <Section>
-        <h1>Contact</h1>
-        <a href="mailto:hello@scampersand.com">hello@scampersand.com</a>
-      </Section>
-      <Footer />
-    </Container>
-  </Landing>
+    <Landing title={siteMetadata.title}>
+      <Container>
+        <Header />
+        <Intro>{siteMetadata.description}</Intro>
+        <Section>
+          <h1>Work</h1>
+          <Project
+            title="Tizra Quickstart"
+            image={projectImages['tizra-quickstart']}
+          >
+            <Project.Description>
+              Tizra brought in Scampersand to improve the configurability and
+              mobile experience of their out-of-the-box theme. Our job was to
+              make these improvements on a small budget within an existing
+              codebase. We applied some creative thinking and technical
+              expertise to provide Tizra with a solution that speeds up their
+              onboarding and allows them to pursue scaling their customer base.
+            </Project.Description>
+          </Project>
+          <Project
+            title="Appsembler Reporting"
+            image={projectImages['appsembler-reports']}
+          >
+            <Project.Description>
+              Appsembler wanted to know how many learners were actively using
+              their Open edX LMS. To give them insight into engagement and
+              customer ROI, we built a secure usage tracker and then backfilled
+              it with historical data. Previously inaccessible information
+              became charts revealing trends and entirely new conversations for
+              the customer success team to pursue. Our work on this ultimately
+              became the foundation for Appsembler's customer-facing reporting
+              tool, Figures.
+            </Project.Description>
+            <Project.Testimonial from="Aaron Beals, Appsembler VP Eng">
+              I have to say, the amount you were able to get done with two
+              people in this timeframe is amazing.
+            </Project.Testimonial>
+          </Project>
+          <Project
+            title="Einstein Search"
+            image={projectImages['einstein-search']}
+          >
+            <Project.Description>
+              We worked with the Einstein Papers Project to improve the
+              searchability of their online archive. Based on extensive feedback
+              from researchers, we created a new UI that matches their
+              terminology and workflow. The new interface is also
+              mobile-friendly and includes explanations of commonly used
+              abbreviations to make the archive more approachable to new users.
+            </Project.Description>
+            <Project.Award name="SIIA CODiE Award finalist" />
+          </Project>
+        </Section>
+        <Section>
+          <h1>Clients</h1>
+          <LogoGrid logos={clientLogos} />
+        </Section>
+        <Section>
+          <h1>Contact</h1>
+          <a href="mailto:hello@scampersand.com">hello@scampersand.com</a>
+        </Section>
+        <Footer />
+      </Container>
+    </Landing>
   )
 }
 
