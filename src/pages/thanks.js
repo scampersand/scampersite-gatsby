12b0767829d7d1352030ca034852af62c49d9a0b
Aron Griffis <aron@scampersand.com>
2018-12-22 17:11:24 -0500

    add thanks page

---
+++ src/pages/thanks.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Page from '../components/Page'
+import Section from '../components/Section'
+
+const ThanksPage = () => (
+  <Page title="Thanks">
+    <Section>
+      <h1>Thanks!</h1>
+      <p>
+        We'll be in touch soon.
+      </p>
+    </Section>
+  </Page>
+)
+
+export default ThanksPage
