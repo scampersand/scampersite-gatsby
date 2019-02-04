605d95a50f489c3b6bdd144a737f067a9dd9fca4
Aron Griffis <aron@scampersand.com>
2019-02-04 16:28:58 -0500

    wider container for large desktops

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,5 +2,5 @@ import React from 'react'
 import {Box} from '.'
 
 export const Container = props => (
-  <Box {...props} mx="auto" px={'1rem'} css={{maxWidth: '1024px'}} />
+  <Box {...props} mx="auto" px={'1rem'} css={{maxWidth: '1880px'}} />
 )
