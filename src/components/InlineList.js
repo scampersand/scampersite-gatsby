57f4532336fe8f90ba47b2d61c256b6a7cd1f0e4
Aron Griffis <aron@scampersand.com>
2019-02-04 22:49:32 -0500

    make InlineList use spacing scale

--- src/components/InlineList.js
+++ src/components/InlineList.js
@@ -1,17 +1,19 @@
 import React from 'react'
+import {Box} from '.'
 
-export const InlineList = ({children, ...props}) => (
-  <ul css={{listStyle: 'none', marginLeft: 0}}>
-    {React.Children.map(children, (child, i) =>
-      <li
+export const InlineList = ({children, gutter = 1, ...props}) => (
+  <Box as="ul" css={{listStyle: 'none', marginLeft: 0}}>
+    {React.Children.map(children, (child, i) => (
+      <Box
+        as="li"
         key={i}
+        mr={i < children.length - 1 ? gutter : 0}
         css={{
           display: 'inline-block',
-          marginRight: i < children.length - 1 ? '1rem' : 0,
         }}
       >
         {child}
-      </li>
-    )}
-  </ul>
+      </Box>
+    ))}
+  </Box>
 )
