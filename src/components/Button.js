24c49110f3ab7ee7e1ab5f9991d90c7dec58c44b
Aron Griffis <aron@scampersand.com>
2019-02-22 18:25:15 -0500

    change padding in button

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,13 @@ import {Button as BaseButton} from '@rebass/emotion'
 import {Text} from '.'
 
 export const Button = ({children, ...props}) => (
-  <BaseButton px={1} py={1} {...props}>
-    <Text variant="titleSans" fontSize="sansSmall">{children}</Text>
+  <BaseButton px={0} py={0} {...props}>
+    <Text
+      variant="titleSans"
+      fontSize="sansSmall"
+      px="2.5em"
+      py="1.25em"
+      children={children}
+    />
   </BaseButton>
 )
