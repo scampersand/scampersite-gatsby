af14361e6dd23f4f66212123709c0b5833495dcc
Aron Griffis <aron@scampersand.com>
2019-03-09 16:17:22 -0500

    set contact form next url from window.location

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -12,28 +12,49 @@ const InputContact = ({label, ...props}) => {
   )
 }
 
-export const ContactForm = () => (
-  <form action="https://formspree.io/hello@scampersand.com" method="POST">
-    <FlexGrid columns={{phone: 1, ipadl: 2}} gutter="20px">
-      <InputContact label="Name" type="text" name="name" required />
-      <InputContact label="Email" type="email" name="_replyto" required />
-      <FlexGrid.Col span={{phone: 1, ipadl: 2}}>
-        <InputContact
-          label="Message"
-          as="textarea"
-          name="body"
-          rows="5"
-          required
-        />
-      </FlexGrid.Col>
-      <FlexGrid.Col span={{phone: 1, ipadl: 2}}>
-        <Button type="submit">
-          <ButtonText>Send</ButtonText>
-        </Button>
-      </FlexGrid.Col>
-    </FlexGrid>
-    <input type="hidden" name="_subject" value="Hello!" />
-    <input type="hidden" name="_next" value="https://scampersand.com/thanks" />
-    <input type="test" name="_gotcha" css={{display: 'none'}} />
-  </form>
-)
+export class ContactForm extends React.Component {
+  state = {
+    next: `https://scampersand.com${this.props.next}`,
+  }
+
+  componentDidMount() {
+    this.setState({
+      next: `${window.location.protocol}//${window.location.host}${this.props.next}`,
+    })
+  }
+
+  render() {
+    const {email} = this.props
+    const {next} = this.state
+    return (
+      <form action={`https://formspree.io/${email}`} method="POST">
+        <FlexGrid columns={{phone: 1, ipadl: 2}} gutter="20px">
+          <InputContact label="Name" type="text" name="name" required />
+          <InputContact label="Email" type="email" name="_replyto" required />
+          <FlexGrid.Col span={{phone: 1, ipadl: 2}}>
+            <InputContact
+              label="Message"
+              as="textarea"
+              name="body"
+              rows="5"
+              required
+            />
+          </FlexGrid.Col>
+          <FlexGrid.Col span={{phone: 1, ipadl: 2}}>
+            <Button type="submit">
+              <ButtonText>Send</ButtonText>
+            </Button>
+          </FlexGrid.Col>
+        </FlexGrid>
+        <input type="hidden" name="_subject" value="Hello!" />
+        <input type="hidden" name="_next" value={next} />
+        <input type="test" name="_gotcha" css={{display: 'none'}} />
+      </form>
+    )
+  }
+}
+
+ContactForm.defaultProps = {
+  email: 'hello@scampersand.com',
+  next: '/thanks',
+}
