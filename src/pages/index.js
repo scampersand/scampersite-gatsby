b60bca6eb9faea89650ed5f9aa9817b74a0aee96
Aron Griffis <aron@scampersand.com>
2019-03-10 19:11:42 -0400

    section header (lede) max length

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,16 +20,11 @@ import {spy} from '../theme'
 const SectionHeader = ({title, children, ...props}) => (
   <Box {...props}>
     <H1>{title}</H1>
-    <Lede>{children}</Lede>
+    <Lede maxWidth={{phone: 'none', laptop: '90%'}}>{children}</Lede>
   </Box>
 )
 
-const IndexPage = ({
-  data: {
-    projectImages,
-    clientLogos,
-  },
-}) => {
+const IndexPage = ({data: {projectImages, clientLogos}}) => {
   projectImages = namedImages(projectImages)
   return (
     <Page>
@@ -38,7 +33,8 @@ const IndexPage = ({
       <Section id="work" first>
         <Container>
           <SectionHeader title="Recent Work">
-            Some of our favorite projects and the amazing clients behind&nbsp;them.
+            Some of our favorite projects and the amazing clients
+            behind&nbsp;them.
           </SectionHeader>
           <Project.Group alternating>
             <Project
@@ -56,14 +52,13 @@ const IndexPage = ({
             >
               <Project.Description>
                 Appsembler wanted their customers to know how learners were
-                using their <Link href="open-edx">Open edX</Link> LMS. To
-                give them insight into engagement and ROI, we built
-                a secure usage tracker and backfilled it with historical
-                data. Previously inaccessible info became charts revealing
-                trends and entirely new conversations for the customer
-                success team to pursue. Appsembler used this foundation to
-                build their customer-facing reporting tool,{' '}
-                <Link href="figures">Figures</Link>.
+                using their <Link href="open-edx">Open edX</Link> LMS. To give
+                them insight into engagement and ROI, we built a secure usage
+                tracker and backfilled it with historical data. Previously
+                inaccessible info became charts revealing trends and entirely
+                new conversations for the customer success team to pursue.
+                Appsembler used this foundation to build their customer-facing
+                reporting tool, <Link href="figures">Figures</Link>.
               </Project.Description>
               <Project.Testimonial from="Aaron Beals, VP Engineering at Appsembler">
                 &ldquo;I have to say, the amount you were able to get done with
@@ -90,12 +85,12 @@ const IndexPage = ({
             >
               <Project.Description>
                 Tizra brought in Scampersand to improve the configurability and
-                mobile experience of their Quickstart theme. Our job was to
-                make these improvements on a limited budget within an
-                existing codebase. We applied some creative thinking and
-                technical expertise to provide Tizra with a solution that
-                speeds up their onboarding and helps their customers start
-                publishing more quickly.
+                mobile experience of their Quickstart theme. Our job was to make
+                these improvements on a limited budget within an existing
+                codebase. We applied some creative thinking and technical
+                expertise to provide Tizra with a solution that speeds up their
+                onboarding and helps their customers start publishing more
+                quickly.
               </Project.Description>
             </Project>
 
@@ -130,8 +125,8 @@ const IndexPage = ({
         <Section id="clients">
           <Container>
             <SectionHeader title="Our Clients">
-              We work with clients who are helping people to learn, grow
-              and contribute to their&nbsp;communities.
+              We work with clients who are helping people to learn, grow and
+              contribute to their&nbsp;communities.
             </SectionHeader>
             <LogoGrid />
           </Container>
@@ -141,8 +136,8 @@ const IndexPage = ({
       <Section id="contact">
         <Container>
           <SectionHeader title="Contact Us">
-            Do you have a project where we could help?
-            Say hello and let's put our heads&nbsp;together.
+            Do you have a project where we could help? Say hello and let's put
+            our heads&nbsp;together.
           </SectionHeader>
           <ContactForm />
         </Container>
